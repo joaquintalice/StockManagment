69ec63806668198bd808811e29aadfc872ee76fe
Joaquin Talice <joaquintalice@gmail.com>
2023-09-16 15:08:47 -0300

    feat: controller logic && service inyected

---
+++ src/warehouse/warehouse.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { WarehouseService } from './warehouse.service';
+import CreateWarehouseDto from './dto/createWarehouse.dto';
+import { UpdateWarehouseDto } from './dto/updateWarehouse.dto';
+
+@Controller('warehouse')
+export class WarehouseController {
+  constructor(private readonly warehouseService: WarehouseService) { }
+
+  @Post()
+  createWarehouse(@Body() createWarehouseDto: CreateWarehouseDto) {
+    return this.warehouseService.create(createWarehouseDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.warehouseService.getAll()
+  }
+
+  @Get('/:id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.warehouseService.getById(id)
+  }
+
+  @Patch('/:id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateWarehouseDto: UpdateWarehouseDto) {
+    return this.warehouseService.update(id, updateWarehouseDto);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.warehouseService.delete(id);
+  }
+
+
+}
