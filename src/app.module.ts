f357a5c150ee75cd8c2d4ce0dd4349aff1e05331
Joaquin Talice <joaquintalice@gmail.com>
2023-09-17 00:20:36 -0300

    feat: products and warehouse providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { WarehouseModule } from './warehouse/warehouse.module';
+import { ProductsModule } from './products/products.module';
+import { PrismaService } from './db/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    WarehouseModule,
+    ProductsModule,
 
   ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule { }
