af9779dc8487ba1a7e8f5473e8040f9024b8d094
Joaquin Talice <joaquintalice@gmail.com>
2023-09-17 12:53:38 -0300

    feat: product controller endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import CreateProductDto from './dto/createProduct.dto';
+import { UpdateProductDto } from './dto/updateProduct.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
+
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto) {
+    return await this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  async getAll() {
+    return await this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', ParseIntPipe) id: number) {
+    return await this.productsService.getById(id);
+  }
+
+  @Patch('/:id')
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+    return await this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return await this.productsService.delete(id);
+  }
+
+
 }
