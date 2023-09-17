949205fbbc11db62373e42fe76f90edd3aa3dd5b
Joaquin Talice <joaquintalice@gmail.com>
2023-09-17 00:02:04 -0300

    feat: warehouses schema update && DTO update too

--- src/warehouse/dto/createWarehouse.dto.ts
+++ src/warehouse/dto/createWarehouse.dto.ts
@@ -1,13 +1,18 @@
-import { IsOptional, IsString } from "class-validator"
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
 
 export default class CreateWarehouseDto {
 
 
     @IsString()
+    @IsNotEmpty()
     name: string
 
     @IsOptional()
     @IsString()
     description?: string
+
+    @IsString()
+    @IsNotEmpty()
+    ubication: string
 
 }
