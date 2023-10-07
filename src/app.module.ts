49b7224544b57c5bc397433005112b7d7cd3ba5a
Joaquin Talice <joaquintalice@gmail.com>
2023-10-06 22:57:39 -0300

    feat: cashbox module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { WarehouseModule } from './warehouse/warehouse.module';
 import { ProductsModule } from './products/products.module';
 import { PrismaService } from './db/prisma.service';
+import { StockmovementModule } from './stockmovement/stockmovement.module';
+import { SmdetailsModule } from './smdetails/smdetails.module';
+import { CashboxModule } from './cashbox/cashbox.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), WarehouseModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    WarehouseModule,
+    ProductsModule,
+    StockmovementModule,
+    SmdetailsModule,
+    CashboxModule
+  ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [
+    PrismaService
+  ],
 })
 export class AppModule { }
